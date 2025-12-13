81cddc8ea11a818b9a630af991e16b3eb6149baf
Mourette Ferdinand <mouretteferdinand@yahoo.com>
2025-12-13 14:44:41 +0100

    Good enough

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,51 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    private float dollars = 100000f;
+    public float dollars = 100000f;
     private int costOfBall = 100;
     [SerializeField] TextMeshProUGUI textMoney;
+    [SerializeField] GameObject DeathScreen;
     private void Start()
     {
-        dollars = 100000;
         UpdateMoneyText();
     }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Echap();
+        }
+    }
+
 
     public void GainMoney(float mult)
     {
         dollars += costOfBall* mult;
+        if (dollars <= 0)
+        {
+            dollars = 0;
+            Echap();
+        }
+
         UpdateMoneyText();
     }
 
     private void UpdateMoneyText()
     {
         textMoney.text = dollars.ToString() + " $";
+    }
+
+    public void Echap()
+    {
+        DeathScreen.SetActive(true);
+    }
+
+
+    public void Home()
+    {
+        SceneManager.LoadScene("Mainmenu");
     }
 }
--- Assets/Scripts/ZoneWinLose.cs
+++ Assets/Scripts/ZoneWinLose.cs
@@ -4,11 +4,19 @@ public class ZoneWinLose : MonoBehaviour
 {
     [SerializeField] GameManager gameManager;
     [SerializeField] float mult = 0.5f;
+    private ParticleSystem ParticleSystem;
+
+    private void Start()
+    {
+        ParticleSystem = GetComponent<ParticleSystem>();
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Ball"))
         {
+            ParticleSystem.Play();
             gameManager.GainMoney(mult);
             Destroy(collision.gameObject);
         }
