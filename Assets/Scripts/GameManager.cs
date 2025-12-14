65b9e4bd7d8c6b43464c658b0ebf384a5b5d8a97
Mourette Ferdinand <mouretteferdinand@yahoo.com>
2025-12-14 15:53:38 +0100

    Latest Change

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -49,4 +49,9 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene("Mainmenu");
     }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("Game");
+    }
 }
